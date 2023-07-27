95528585dd7b477a3d1d445ab4318649477b8fc7
david0411 <tlyeyd@gmail.com>
2023-07-27 12:24:02 +0800

    bug fix

--- src/main/java/fsse2305/eshop/config/EnvConfig.java
+++ src/main/java/fsse2305/eshop/config/EnvConfig.java
@@ -2,5 +2,5 @@ package fsse2305.eshop.config;
 
 public class EnvConfig {
     public static final String devConfig = "http://localhost:5173";
-    public static final String prodConfig = "http://fsse2305.project.david.cheng.s3-website-ap-southeast-1.amazonaws.com/";
+    public static final String prodConfig = "http://fsse2305.project.david.cheng.s3-website-ap-southeast-1.amazonaws.com";
 }
